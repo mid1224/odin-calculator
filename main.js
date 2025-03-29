f86fe9e5df3d3cbe2280d3c90acf4904868f1557
mid1224 <61130938+mid1224@users.noreply.github.com>
2025-03-29 21:49:29 +0700

    Add negative & decimal input & backspace

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-let num1, num2, operator = 0;
+let num1, num2, operator = undefined;
 let displayingResult = false;
 
-const digitButtons = document.querySelectorAll('.digit');
-const operatorButtons = document.querySelectorAll('.operator:not(#equal):not(#clear)');
+const digitButtons = document.querySelectorAll('.digit:not(#negative):not(#decimal)');
+const operatorButtons = document.querySelectorAll('.operator:not(#equal):not(#clear):not(#backspace)');
+const negativeButton = document.querySelector('#negative');
+const decimalButton = document.querySelector('#decimal');
 const equalButton = document.querySelector('#equal');
+const backspaceButton = document.querySelector('#backspace');
 const clearButton = document.querySelector('#clear');
 const display = document.querySelector('#display');
 
@@ -20,6 +23,24 @@ digitButtons.forEach(button => {
     });
 });
 
+negativeButton.addEventListener('click', function() {
+    if (display.textContent === "" || displayingResult === true) {
+        display.textContent = "-";
+        displayingResult = false;
+    }
+});
+
+decimalButton.addEventListener('click', function() {
+    if (display.textContent === "" || displayingResult === true) {
+        display.textContent = "0.";
+        displayingResult = false;
+    } else if (display.textContent === "-") {
+        display.textContent = "-0.";
+    } else if (!display.textContent.includes('.')) {
+        display.textContent += ".";
+    }
+});
+
 operatorButtons.forEach(button => {
     button.addEventListener('click', function() {
         if (display.textContent !== '') {
@@ -31,21 +52,34 @@ operatorButtons.forEach(button => {
 });
 
 equalButton.addEventListener('click', function(event) {
-    num2 = parseFloat(display.textContent);
-    let result = operate(num1, num2, operator);
-    if (result === null) {
-        display.textContent = "Error";
-    } else {
-        display.textContent = result;
-    }
+    if (displayingResult === false)
+    {
+        num2 = parseFloat(display.textContent);
+        let result = operate(num1, num2, operator);
+        if (result === null) {
+            display.textContent = "Error";
+        } else {
+            display.textContent = result;
+        }
 
-    displayingResult = true;
+        num1 = 0;
+        num2 = 0;
+        operator = "+";
+
+        displayingResult = true;
+    }
+});
+
+backspaceButton.addEventListener('click', function(event) {
+    if (display.textContent.length !== "" && displayingResult === false) {
+        display.textContent = display.textContent.slice(0, -1);
+    }
 });
 
 clearButton.addEventListener('click', function(event) {
     display.textContent = "";
-    num1 = 0;
-    num2 = 0;
+    num1 = undefined;
+    num2 = undefined;
     operator = "";
     displayingResult = false;
 });
